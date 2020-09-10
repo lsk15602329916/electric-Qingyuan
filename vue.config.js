c6f775e34cf5c415ce4cb0369adbfabac3034db1
P_juan <1040589574@qq.com>
2020-09-10 08:14:33 +0800

    添加 vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const resolve = dir => path.join(__dirname, dir);
+
+module.exports = {
+    chainWebpack: config => {
+        // 添加别名
+        config.resolve.alias
+            .set("vue$", "vue/dist/vue.esm.js")
+            .set("@", resolve("src"))
+            .set("@assets", resolve("src/assets"))
+            .set("@components", resolve("src/components"))
+            .set("@plugins", resolve("src/plugins"))
+            .set("@views", resolve("src/views"))
+            .set("@router", resolve("src/router"))
+            .set("@store", resolve("src/store"))
+            .set("@static", resolve("src/static"));
+    }
+};
